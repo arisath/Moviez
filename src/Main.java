517f917a32bd2633fd67f3e041284201f79723d8
arisath <arist.athanas@gmail.com>
2016-10-17 21:56:59 +0100

    Created the getMovieJson and encodeURL functions

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println(Moviez.getMovieJson("Reservoir Dogs"));
+
+    }
+
+}
--- src/Moviez.java
+++ src/Moviez.java
@@ -1,26 +1,48 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
-public class Main
+public class Moviez
 {
-    public static void main(String [ ] args)   throws Exception
+    static final String omdbapi = "http://www.omdbapi.com/?t=";
+
+    static String getMovieJson(String movieTitle)
     {
+        try
+        {
+            URL url = new URL(omdbapi + encodeURL(movieTitle) + "&y=&plot=short&r=json");
 
+            URLConnection urlConnection = url.openConnection();
 
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
-                URL yahoo = new URL("http://www.omdbapi.com/?t=Lock%2C+Stock+and+Two+Smoking+Barrels&y=&plot=short&r=json");
-                URLConnection yc = yahoo.openConnection();
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(
-                                yc.getInputStream()));
-                String inputLine;
+            String inputLine;
 
-                while ((inputLine = in.readLine()) != null)
-                    System.out.println(inputLine);
-                in.close();
+            inputLine = in.readLine();
 
+            in.close();
 
+            return inputLine;
+        }
+        catch (MalformedURLException malforedUrlException)
+        {
+            return ("The provided URL is malformed");
+        }
+        catch (IOException malforedUrlException)
+        {
+            return ("The provided URL is not valid");
+        }
+    }
+
+    static String encodeURL(String s) throws UnsupportedEncodingException
+    {
+        String encodedUrl = URLEncoder.encode(s, "UTF-8");
+
+        return encodedUrl;
     }
 }
